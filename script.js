f9450376d58446f3dac60ab0d76e875921c18646
Svetlana Kim <svkim1995@gmail.com>
2024-01-23 13:57:08 +0900

    add files

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const buttonMore = document.querySelector('.more-button');
+    const buttonLess = document.querySelector('.less-button');
+    let brandItems = {};
+    if (window.innerWidth > 1391) {
+        brandItems = document.querySelectorAll('.brands-container__item:nth-child(n+11)');
+    } else if (window.innerWidth > 768) {
+        brandItems = document.querySelectorAll('.brands-container__item:nth-child(n+9)');
+    } else if (window.innerWidth > 320) {
+        brandItems = document.querySelectorAll('.brands-container__item:nth-child(n+7)');
+    } 
+    
+    buttonMore.addEventListener('click', function() {
+        for(let item of brandItems) {
+            item.style.display = 'flex';
+        }
+    
+        buttonMore.style.display = 'none';
+        buttonLess.style.display = 'flex';
+    });
+
+    buttonLess.addEventListener('click', function(){
+        for(let item of brandItems) {
+            item.style.display = 'none';
+        }
+
+        buttonMore.style.display = 'flex';
+        buttonLess.style.display = 'none';
+    })
+    
+});
---
+++ swiper.js
@@ -0,0 +1,18 @@
+const brands = document.querySelector('.brands');
+const brandsContainer = document.querySelector('.brands-container');
+const brandsContainerItems = document.querySelectorAll('.brands-container__item');
+if (window.innerWidth <= 320) {
+  brands.classList.add('swiper');
+  brandsContainer.classList.add('swiper-wrapper');
+  for (let item of brandsContainerItems) {
+    item.classList.add('swiper-slide');
+  }
+};
+
+const swiper = new Swiper('.swiper', { 
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  slidesPerView: 1.25,
+});
